c6ffa25bdc3d0996a5fc7650c61e02e86ce5a989
Paul <arthur.meteng@yahoo.com>
2023-12-02 14:50:35 +0100

    add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'e79fc557-6d59-4294-ac65-3b5acc26b559';
-}
+export class AppComponent {}
