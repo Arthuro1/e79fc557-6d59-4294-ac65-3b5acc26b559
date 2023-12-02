9456de82489b9fec64121e88eca87c80d7ab4559
Paul <arthur.meteng@yahoo.com>
2023-12-02 15:04:12 +0100

    create event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AsyncPipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,17 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    MatFormFieldModule, 
+    MatAutocompleteModule, 
+    ReactiveFormsModule, 
+    AsyncPipe, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
