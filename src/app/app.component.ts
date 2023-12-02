0c37b96388ee154c255e3b16f63aefd9d55b8a68
unknown <arthur.meteng@yahoo.com>
2023-12-02 14:26:12 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'e79fc557-6d59-4294-ac65-3b5acc26b559';
+}
