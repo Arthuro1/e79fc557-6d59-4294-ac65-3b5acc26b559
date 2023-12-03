9f2cb22c64530f9b49457ee93b8859943305613b
Paul <arthur.meteng@yahoo.com>
2023-12-03 23:34:34 +0100

    home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../../services/events.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   title = "TecLead";
+  events: Event[] = [];
+  
+  constructor(private http: HttpClient, private eventsService: EventsService) {}
+  
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.eventsService.getAllEvents()
+    .subscribe((events: any) => {
+      this.events = events;
+    });
+  }
 }
