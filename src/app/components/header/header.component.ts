7a12ea2ab12dc674ac86cd2f6c0084cb17815b65
Paul <arthur.meteng@yahoo.com>
2023-12-03 23:22:05 +0100

    use service for event search

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,9 +6,11 @@ import {MatBadgeModule} from '@angular/material/badge';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {startWith, map} from 'rxjs/operators';
-import {AsyncPipe} from '@angular/common';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { EventsService } from '../../services/events.service';
+import { Event } from '../../models/event.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -17,20 +19,39 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 export class HeaderComponent implements OnInit{ 
   myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
+  options: string[] = [];
   filteredOptions!: Observable<string[]>;
+  events!: Event[];
+
+  constructor(public http: HttpClient, public eventsService: EventsService) { }
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
+    this.getEvents();
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
+    if(value == "") return [];
+
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+  
+  getEvents() {
+    this.eventsService.getAllEvents()
+    .subscribe((events: any) => {
+      this.events = events;
+      console.log("This.getEvents", this.events);
+
+      this.options = this.events.map((event :Event) => {
+        return event.title;
+      });
+
+      this.filteredOptions = this.myControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value || '')),
+      );
+    });
   }
 
 }
