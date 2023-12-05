7f9405eccaca39d57e8b9aa6849bc15d8f2c3d60
Paul <arthur.meteng@yahoo.com>
2023-12-05 02:31:27 +0100

    refactoring

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,52 +16,28 @@ export class HomeComponent implements OnInit{
   startDate: string | undefined;
   endDate: string | undefined;
 
-  constructor(private http: HttpClient, private eventsService: EventsService) {}
+  constructor(private eventsService: EventsService) {}
   
   ngOnInit(): void {
     this.getEvents();
 
-    this.eventsService.change.subscribe(filteredEvents => {
+    this.eventsService.inputChange.subscribe(filteredEvents => {
       this.events = filteredEvents;
-      this.resultData = of(this.groupByDate(filteredEvents));
+      this.resultData = of(this.eventsService.groupByDate(filteredEvents));
     });
   }
 
   getEvents() {
     this.eventsService.getAllEvents()
-    .subscribe((events: any) => {
-      this.events = this.sortByDate(events);
-      this.city = events[0].city;
-      this.startDate = events[0].date;
-      this.endDate = events[events.length - 1].date;
-      
-      console.log("-", this.events);
-      
-      this.resultData = of(this.groupByDate(this.events));
-    });
-  }
-
-  sortByDate(events: Event[]): Event[]{
-    const sortedAscEvents = events.sort(
-      (objA, objB) => new Date(objA.date).getTime() - new Date(objB.date).getTime(), 
-    );
-
-    return sortedAscEvents;
-  }
-
-  groupByDate(events: Event[]): any {
-    const data = new Set(events.map((item: Event) => item.startTime))
-    let resultData: { date: string; events: Event[]; }[] = [];
-    data.forEach((date) => {
-      resultData.push({
-        date: date, 
-        events: events.filter((i: Event) => i.startTime === date)
+      .subscribe((events: any) => {
+        this.events = this.eventsService.sortByDate(events);
+        this.city = events[0].city;
+        this.startDate = events[0].date;
+        this.endDate = events[events.length - 1].date;
+        
+        console.log("-", this.events);
+        
+        this.resultData = of(this.eventsService.groupByDate(this.events));
       });
-    });
-
-    console.log("data", data);
-    console.log("resultData", resultData);
-
-    return resultData;
   }
 }
